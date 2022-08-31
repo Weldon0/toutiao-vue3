9710d8b449eb00c2672a2c8f763be96e4bba0efc
wuxiaohui <374620777@qq.com>
2022-08-31 12:07:27 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <Channel />
+  <NewsList />
+</template>
+<script setup lang="ts">
+import Channel from "./components/Channel.vue";
+import NewsList from "./components/NewsList.vue";
+</script>
---
+++ src/components/NewsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list">
+    <div class="article_item">
+      <h3 class="van-ellipsis">python数据预处理 ：数据标准化</h3>
+      <div class="img_box">
+        <img
+            src="http://geek.itheima.net/resources/images/11.jpg"
+            class="w100"
+            alt=""
+        />
+      </div>
+      <div class="info_box">
+        <span>13552285417</span>
+        <span>0评论</span>
+        <span>2018-11-29T17:02:09</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoed lang="less"></style>
